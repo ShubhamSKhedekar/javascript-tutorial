6174b1efac7a22e4df5df868d3fe73642e70a97b
Shubham Khedekar <125666627+ShubhamSKhedekar@users.noreply.github.com>
2024-07-12 17:53:25 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,14 +1,9 @@
 "use strict"; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs in ide will give error , in browser's console if we run it works fine
+console.log(3 +3) // code readability should be high
 
-console.log(3 
-    +
-     3) // code readability should be high
-
-console.log("Hitesh")
-
-
+console.log("Hitesh");
 let name = "hitesh"
 let age = 18
 let isLoggedIn = false
